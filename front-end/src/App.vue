d09470cf3cb480aee1be54160194d8438400484d
rhdiddl501 <rhdiddl501@naver.com>
2020-04-16 14:39:00 +0900

    add admincheck method

--- front-end/src/App.vue
+++ front-end/src/App.vue
@@ -1,19 +1,32 @@
 <template>
   <div id="app">
-    <form method="post" action="/api/NewFile.jsp">
-      ID: <input type="text" name="loginId"><br/>
-      PWD: <input type="password" name="loginPwd"><br/>
-      <input type="submit" value="log in">
-    </form>
+      ID: <input type="text" name="loginId" v-model="loginId"><br/>
+      PWD: <input type="password" name="loginPwd" v-model="loginPwd"><br/>
+      <button @click="adminCheck(loginId,loginPwd)">log in </button>    
   </div>
 </template>
 
 <script>
+import axios from 'axios'
 export default {
   name: 'app',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      loginId: '',
+      loginPwd: '',
+      isAdmin: 0
+    }
+  },
+  methods: {
+    adminCheck() {
+      let params = new URLSearchParams();
+      params.append('loginId',this.loginId)
+      params.append('loginPwd',this.loginPwd)
+      axios.post('/api/AdminCheck',params).then(res => {
+        this.isAdmin = res.headers.isadmin
+      }).catch((ex) => {
+        console.warn('Error')
+      })
     }
   }
 }
